ec848b7b5a5acfb18d0c221c00192064afb4975b
Filip <dr.ipi95@gmail.com>
2023-05-07 17:50:52 +0200

    spelling

--- tests/loginPage.spec.js
+++ tests/loginPage.spec.js
@@ -2,28 +2,28 @@ import { test, expect } from '@playwright/test';
 import { LoginPage } from "../pages/loginPage";
 
 test.describe('Login page', () => {
-    test('Login unsucessfull - wrong username', async ({ page }) => {
+    test('Login unsuccessful - wrong username', async ({ page }) => {
         const Login = new LoginPage(page)
         await Login.gotoPage()
         await Login.login("not_standard_user", "secret_sauce")
         await Login.validateErrorMessage()
     });
 
-    test('Login unsucessfull - wrong password', async ({ page }) => {
+    test('Login unsuccessful - wrong password', async ({ page }) => {
         const Login = new LoginPage(page)
         await Login.gotoPage()
         await Login.login("standard_user", "not_secret_sauce")
         await Login.validateErrorMessage()
     });
 
-    test('Login unsucessfull - wrong username & password', async ({ page }) => {
+    test('Login unsuccessful - wrong username & password', async ({ page }) => {
         const Login = new LoginPage(page)
         await Login.gotoPage()
         await Login.login("not_standard_user", "not_secret_sauce")
         await Login.validateErrorMessage()
     });
 
-    test('Login sucessfull', async ({ page }) => {
+    test('Login successful', async ({ page }) => {
         const Login = new LoginPage(page)
         await Login.gotoPage()
         await Login.login("standard_user", "secret_sauce")
